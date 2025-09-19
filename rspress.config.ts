df0832e6a6679b20d30c77c6b4108e219ebbcc36
hanyun <1355081829@qq.com>
2025-09-19 22:05:56 +0800

    init

--- rspress.config.ts
+++ rspress.config.ts
@@ -2,6 +2,7 @@ import * as path from 'node:path';
 import { defineConfig } from 'rspress/config';
 
 export default defineConfig({
+  outDir: './dist',
   root: path.join(__dirname, 'docs'),
   title: 'My Site',
   icon: '/rspress-icon.png',
